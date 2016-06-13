1deeaf1983c49f315ba4646ecf21965edc2c1f5c
Samuel GD <nomail@mail.com>
2016-06-13 18:21:41 +0200

    First real commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*-coding:Latin-1 -*
+
+""" Detection of insults in comments """
+
+def preprocessing():
+    pass
+
+def classify():
+    pass
+
+if __name__ == "__main__":
+    print("First commit !")
