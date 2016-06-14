6630322df419050c9e45337cab654a9ef4516fc4
Samuel GD <nomail@mail.com>
2016-06-14 16:19:22 +0200

    Function to count bad words and find bad words ratio

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,35 @@
 # -*-coding:Latin-1 -*
 import numpy as np
+import pandas as pd
 
 """ Data preprocessing """
 
+def bad_words(words, wordlist_fname):
+    """ Returns the number of bad_words and ratio of bad_words """
+
+    badwords = pd.read_csv("badwords.txt", sep = '\n').as_matrix()
+    badwords = np.ndarray.flatten(badwords)
+    
+    mask = np.in1d(words, badwords)
+    
+    count = len(mask == True)
+    ratio = float(count) / len(words)
+    
+    return count, ratio
+    
+def capital_words(words):
+    """ Returns the number of uppercase words """
+    
+    pass
+    
+
 def preprocessing(X):
     """ Create features """
+
+        
     
     X_processed = np.array([len(x) for x in X])
+    
+    
     
     return X_processed
